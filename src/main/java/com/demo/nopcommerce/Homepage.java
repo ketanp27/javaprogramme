11f4bbc4686873ca70903a7b83ae1fad5dcdf2e0
Ketan Patel <ket80@hotmail.co.uk>
2021-10-09 14:34:14 +0100

    this is my first commit

---
+++ src/main/java/com/demo/nopcommerce/Homepage.java
@@ -0,0 +1,16 @@
+package com.demo.nopcommerce;
+
+public class Homepage {
+
+    //Main method to execute the program
+    public static void main(String[] args) {
+
+        //Print statement to print some text.
+        System.out.println("My First Java Program");
+
+   System.out.println("Learning git");
+
+    }
+
+}
+
